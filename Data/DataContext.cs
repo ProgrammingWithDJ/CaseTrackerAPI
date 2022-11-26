d25f5fc9952eaf3ef80c52e4f0c75d5406ef7475
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-11-26 21:31:48 +0530

    Added Case controller and Datacontext and created the caseTable

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using CaseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseTracker.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Case> Cases{get;set;}
+    }
+}
---
+++ Models/Case.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CaseTracker.Models
+{
+    public class Case
+    {
+        public int Id { get; set; }
+        public int CaseNumber { get; set; }
+
+        public string Workload { get; set; }
+
+        public int Active { get; set;}
+
+        public string Region { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime DateOfArrival { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime CloseDate { get; set; }
+
+        public int Survey { get; set; }
+
+    }
+}
