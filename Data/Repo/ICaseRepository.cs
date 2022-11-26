2646c91fec65f73e495feea69a98c7332fc35dfc
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-11-26 22:24:40 +0530

    Removed the dependency on DataContext and Implemented the repository pattern

---
+++ Data/Repo/ICaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using CaseTracker.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CaseTracker.Data.Repo
+{
+    public interface ICaseRepository
+    {
+        Task<IEnumerable<Case>> GetCasesAsync();
+
+        void AddCase(Case casess);
+
+        Task<bool> SaveAsync();
+    }
+}
