08d78fd2840fd540697ace81416658674d57a245
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-11-29 21:12:56 +0530

    Added few required field validators

--- Dtos/CaseDtos.cs
+++ Dtos/CaseDtos.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CaseTracker.Dtos
 {
     public class CaseDtos
     {
-    
+
+            [Required (ErrorMessage ="Case number is Required")]
+             
             public int CaseNumber { get; set; }
 
+            [Required]
             public string Workload { get; set; }
-
+            
             public int Active { get; set; }
 
+            [Required]
             public string Region { get; set; }
 
             [Column(TypeName = "Date")]
@@ -20,7 +25,6 @@ namespace CaseTracker.Dtos
 
             [Column(TypeName = "Date")]
             public DateTime CloseDate { get; set; }
-
             public int Survey { get; set; }
         }
 
