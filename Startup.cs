8155bdfbc691fadd748c4e87c0ba9827a204b220
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-12-01 20:56:11 +0530

    Refactored the code to a single file

--- Startup.cs
+++ Startup.cs
@@ -45,9 +45,9 @@ namespace CaseTracker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.ConfigureExceptionHandler(env);
+            app.ConfigureExceptionHandler(env);
 
-            app.UseMiddleware<ExceptionMiddleware>();
+            
             app.UseRouting();
 
             app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); 
