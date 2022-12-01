e8f422abdd9707814517836e51625743f7a98704
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-12-01 20:54:32 +0530

    Updated the code to include Custom Middleware

--- Middlewares/ExceptionMiddleware.cs
+++ Middlewares/ExceptionMiddleware.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CaseTracker.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CaseTracker.Middlewares
@@ -9,11 +12,14 @@ namespace CaseTracker.Middlewares
     {
         private readonly RequestDelegate next;
         private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
 
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger
+            , IHostEnvironment env)
         {
             this.next = next;
             this.logger = logger;
+            this.env = env;
         }
 
         public async Task Invoke(HttpContext context) {
@@ -23,9 +29,26 @@ namespace CaseTracker.Middlewares
             }
             catch(Exception ex)
             {
+                ApiErrors response;
+                HttpStatusCode statusCode= HttpStatusCode.InternalServerError;
+
+                String message;
+
+                var exceptionType = ex.GetType();
+
+                if(env.IsDevelopment())
+                {
+                    response = new ApiErrors((int)statusCode,ex.Message,ex.StackTrace.ToString());
+                }
+                else
+                {
+                    response = new ApiErrors((int)statusCode, ex.Message);
+                }
+
                 logger.LogError(ex,ex.Message);
-                context.Response.StatusCode = 500;
-                await context.Response.WriteAsync(ex.Message);
+                context.Response.StatusCode = (int)statusCode;
+                context.Response.ContentType= "application/json";
+                await context.Response.WriteAsync(response.ToString());
             }
         }
     }
